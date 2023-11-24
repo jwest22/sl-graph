1735216820cf65a7358c09a19a77137f423ec17f
Jake <jweststewart@gmail.com>
2023-11-24 09:49:24 -0700

    init commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import streamlit.components.v1 as components
+import networkx as nx
+from pyvis.network import Network
+import network_graph
+
+st.title('Data Symmetry')
+
+if 'physics-key' not in st.session_state:
+    st.session_state['physics-key'] = False
+    
+if 'hierarchy-key' not in st.session_state:
+    st.session_state['hierarchy-key'] = False
+
+if st.toggle('Enable Physics', value=st.session_state['physics-key']):
+    st.session_state['physics-key']=True
+else:
+    st.session_state['physics-key']=False
+
+if st.toggle('Enable Hierarchy Layout', value=st.session_state['hierarchy-key']):
+    st.session_state['hierarchy-key']=True
+else:
+    st.session_state['hierarchy-key']=False
+    
+network_graph.network_func(
+                           st.session_state['physics-key'],
+                           st.session_state['hierarchy-key']
+                           )
+
+HtmlFile = open("front.html", 'r', encoding='utf-8')
+source_code = HtmlFile.read() 
+components.html(source_code, height = 1200,width=1000)
---
+++ network_graph.py
@@ -0,0 +1,83 @@
+import networkx as nx
+from pyvis.network import Network
+import pandas as pd
+
+def network_func(physics,layout):
+  
+  network_net = Network(
+                        height="600px", 
+                        width="100%", 
+                        bgcolor='#222222',
+                        font_color="white",
+                        directed=True,
+                        layout=layout,
+                        heading=''
+                        )
+  
+  network_net.repulsion(
+                        node_distance=420,
+                        central_gravity=0.33,
+                        spring_length=110,
+                        spring_strength=0.10,
+                        damping=0.95
+                       )
+
+  network_net.barnes_hut()
+  network_data = pd.read_csv("graphdata.csv")
+
+  sources = network_data['Source']
+  targets = network_data['Target']
+  weights = network_data['Weight']
+  src_groups = network_data['Source_Group']
+  trg_groups = network_data['Target_Group']
+
+  edge_data = zip(
+                  sources, 
+                  targets, 
+                  weights, 
+                  src_groups, 
+                  trg_groups
+                  )
+
+  for e in edge_data:
+    src = e[0]
+    dst = e[1]
+    w = e[2]
+    sg = e[3]
+    tg = e[4]
+
+    network_net.add_node(
+                         n_id=src, 
+                         label=src, 
+                         title=src,
+                         level=sg,
+                         group=sg,
+                         physics=physics
+                        )
+    network_net.add_node(
+                         n_id=dst, 
+                         label=dst, 
+                         title=dst,
+                         group=tg,
+                         level=tg,
+                         physics=physics
+                        )
+    network_net.add_edge(
+                         source=src, 
+                         to=dst, 
+                         title= src + ' to ' + dst + ' symmetry: ' + str(w) + '%',
+                         value=w,
+                         physics=physics,
+                         arrowStrikethrough=True
+                        )
+
+  neighbor_map = network_net.get_adj_list()
+
+  for node in network_net.nodes:
+    node["title"] += ' Neighbors: ' + ', '.join(neighbor_map[node["id"]])
+    node["value"] = len(neighbor_map[node["id"]])
+  if physics:
+    network_net.show_buttons(
+                             filter_=['physics']
+                            )
+  network_net.save_graph("front.html")
